df6eb88600b616a644c0e46eb9028eeeacf6975c
Kevin <hihellohi610@gmail.com>
2016-09-05 15:54:11 +1000

    drop module

--- sender.cpp
+++ sender.cpp
@@ -11,11 +11,11 @@
 
 #include <queue>
 #include <string>
+#include <algorithm>
 
 #include "header.h"
 
-typedef struct sockaddr_in sockaddr_in;
-typedef struct sockaddr sockaddr;
+static double dropchance;
 
 void die(std::string s){
 	perror(s.c_str());
@@ -23,7 +23,7 @@ void die(std::string s){
 }
 
 int tryrecv(int s, char *buf, int bufsize, sockaddr *si_target, int *slen, int us){
-	struct timeval timeout;
+	timeval timeout;
 	memset((char*)&timeout, 0, sizeof(timeout));
 	timeout.tv_usec = us;
 	
@@ -46,7 +46,10 @@ int tryrecv(int s, char *buf, int bufsize, sockaddr *si_target, int *slen, int u
 }
 
 int trysend(int s, char *buf, int buffsize, sockaddr *si_target, int slen){
-	return sendto(s, buf, buffsize, 0, si_target, slen);
+	if(rand()/(((double)RAND_MAX + 1)) > dropchance){
+		sendto(s, buf, buffsize, 0, si_target, slen);
+	}
+	return buffsize;
 }
 
 int make_packet(char* buf, int buffsize, unsigned int *n_seq, FILE *fin) {
@@ -58,6 +61,22 @@ int make_packet(char* buf, int buffsize, unsigned int *n_seq, FILE *fin) {
 	*n_seq += ((Header)buf)->len;
 	((Header)buf)->flags = (1 << DATA);
 	return 1;
+}
+
+void set_timer(timeval *tv){
+	gettimeofday(tv, 0);
+}
+
+int get_timer(timeval *tv){
+	timeval tmp;
+	gettimeofday(&tmp, 0);
+	int now = tmp.tv_sec * 1000 + tmp.tv_usec / 1000;
+	int before = tv->tv_sec * 1000 + tv->tv_usec / 1000;
+	int out = now - before;
+	if(out < 0){
+		out += 24 * 3600000;
+	}
+	return out;
 }
 
 int main(int argc, char **argv){
@@ -74,6 +93,8 @@ int main(int argc, char **argv){
 	int mws = atoi(argv[4]);
 	int mss = atoi(argv[5]);
 	int timeout = atoi(argv[6]) * 1000;
+	dropchance = atof(argv[7]);
+	srand(atoi(argv[8]));
 
 	sockaddr_in si_other;
 
@@ -98,6 +119,9 @@ int main(int argc, char **argv){
 	std::queue<char*> q;
 	unsigned int seq = 0;
 
+	timeval timer;
+	int fast = 0;
+
 	while(!q.empty() || !feof(fin)){
 
 		while((int)q.size() < mws && make_packet(buf, buffsize, &seq, fin)){
@@ -105,19 +129,43 @@ int main(int argc, char **argv){
 			
 			printf("sending packet #%u\n", ((Header)buf)->n_seq);
 			trysend(s, buf, sizeof(header) + ((Header)buf)->len, (sockaddr*)&si_other, slen);
+
+			if(q.size() == 1){
+				set_timer(&timer);
+			}
 		}
 
-		n = tryrecv(s, buf, buffsize, (sockaddr*)&si_other, &slen, timeout);
+		n = tryrecv(s, buf, buffsize, (sockaddr*)&si_other, &slen, std::max(0, timeout - get_timer(&timer)));
 		if(n == -2){
+
+			printf("timeout\n");
 			printf("resending packet #%u\n", ((Header)q.front())->n_seq);
 			trysend(s, q.front(), sizeof(header) + ((Header)q.front())->len, (sockaddr*)&si_other, slen);
-			printf("timeout\n");
+
+			set_timer(&timer);
 			continue;
 		}
 		else if(((Header)buf)->flags & (1 << ACK)){
-			printf("Received ACK #%u from %s:%d\n", ((Header)buf)->n_ack, inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+			printf("Received ACK #%u from %s:%d\n", 
+					((Header)buf)->n_ack, 
+					inet_ntoa(si_other.sin_addr), 
+					ntohs(si_other.sin_port));
+
+			if(!q.empty() && ((Header)buf)->n_ack == ((Header)q.front())->n_seq){
+				if(++fast > 2){
+					fast = 0;
+
+					printf("preemptive timeout\n");
+					printf("resending packet #%u\n", ((Header)q.front())->n_seq);
+					trysend(s, q.front(), sizeof(header) + ((Header)q.front())->len, (sockaddr*)&si_other, slen);
+					set_timer(&timer);
+				}
+			}
 
 			while(!q.empty() && ((Header)buf)->n_ack > ((Header)q.front())->n_seq){
+				fast = 0;
+				set_timer(&timer);
+
 				free(q.front());
 				q.pop();
 			}
